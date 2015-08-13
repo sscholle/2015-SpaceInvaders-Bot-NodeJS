f8a6f56b20277f9cd8a1f7f7186231ce959b0e5f
Sebastian Scholle <sebastian@rightshift.biz>
2015-08-13 17:42:12 +0200

    Notes for Dev

--- src/main.js
+++ src/main.js
@@ -15,7 +15,51 @@ function main(outputPath) {
     var map = mapLoader.load(outputPath);
     logMap(map);
 
-    var move = getRandomMove();
+    var move = getRandomMove(); // this is where we need to figure out how best to move
+
+// priority object
+/**
+
+**/
+
+/** logic tree
+
+// can shoot (missileLimitReached?)
+targeting opportities (prioritised)
+
+// can build? (hasLivesLeft?)
+Building priorities (priorities)
+
+// movement required?
+'may need to move to build or move to avoid fire'
+**/
+
+/**
+util functions
+
+TARGETING FUNCTIONS:
+decide what is weakest target (easiest , last amount of moves and least amount of turns) --- using the following util functions
+calc where to shoot in order to hit moving alien
+* hit alien factory
+* hit missile controller
+    *** include priorities that show least amount of shields --- and calculate if aliens will cross paths
+
+BUILDING FUNCTIONS:
+decide what is fastest bonus to build (static build priority list)
+
+
+// build list: 
+1. Missile Controller
+2. Alien Factory
+
+DEFENSE CONSIDERATIONS:
+'when to build shields'
+note that shields block all bullets,missiles and aliens (even yours i suppose)
+and
+'when to move to avoid enemy fire'
+**/
+
+
     util.outputMove(move, outputPath, function() {
         var endTime = moment();
         var runTime = endTime.diff(startTime);
